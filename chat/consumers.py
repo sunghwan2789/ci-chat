a0f28b54312ecf05ba199a6b9581fb4c55b79b80
Sunghwan Bang <sunghwan2789@gmail.com>
2019-08-09 12:36:09 +0000

    공백 제거

--- chat/consumers.py
+++ chat/consumers.py
@@ -47,14 +47,13 @@ def ws_receive(message):
         label = message.channel_session['room']
         room = Room.objects.get(label=label)
         log.debug('recieved message, room exist label=%s', room.label)
- 
     except KeyError:
         log.debug('no room in channel_session')
         return
     except Room.DoesNotExist:
         log.debug('recieved message, buy room does not exist label=%s', label)
         return
- 
+
     # Parse out a chat message from the content text, bailing if it doesn't
     # conform to the expected message format.
     try:
@@ -62,16 +61,16 @@ def ws_receive(message):
     except ValueError:
         log.debug("ws message isn't json text=%s", text)
         return
-     
+
     if set(data.keys()) != set(('handle', 'message')):
         log.debug("ws message unexpected format data=%s", data)
         return
- 
+
     if data:
         log.debug('chat message room=%s handle=%s message=%s', 
             room.label, data['handle'], data['message'])
         m = room.messages.create(**data)
- 
+
         # See above for the note about Group
         Group('chat-'+label, channel_layer=message.channel_layer).send({'text': json.dumps(m.as_dict())})
  
