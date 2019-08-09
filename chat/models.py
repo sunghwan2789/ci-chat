3a08fcba11d36aeba5a047a8a9a59b9c29ee8e5c
Sunghwan Bang <sunghwan2789@gmail.com>
2019-08-09 14:36:42 +0000

    모델 관리자 페이지 스트링 추가

--- chat/models.py
+++ chat/models.py
@@ -1,27 +1,25 @@
-
-
 from django.db import models
 from django.utils import timezone
 
 class Room(models.Model):
     name = models.TextField()
     label = models.SlugField()
-    
-    def __unicode__(self):
+
+    def __str__(self):
         return self.label
 
 class Message(models.Model):
-    room = models.ForeignKey(Room,related_name='messages',on_delete=models.CASCADE )
+    room = models.ForeignKey(Room, related_name='messages', on_delete=models.CASCADE)
     handle = models.TextField()
     message = models.TextField()
     timestamp = models.DateTimeField(default=timezone.now, db_index=True)
-    
-    def __unicode__(self):
+
+    def __str__(self):
         return '[{timestamp}] {handle}: {message}'.format(**self.as_dict())
-    
+
     @property
     def formatted_timestamp(self):
         return self.timestamp.strftime('%b %-d %-I:%M %p')
-    
+
     def as_dict(self):
         return {'handle': self.handle, 'message': self.message, 'timestamp': self.formatted_timestamp}
